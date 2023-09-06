35b7a99d798857a35ffbf178ab719ccc79bd3f51
Enes Altuntaş <46319966+Enes-Altuntas@users.noreply.github.com>
2023-09-06 17:44:22 +0300

    tailwind

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,38 +1,36 @@
 import Link from 'next/link';
-import styles from "./header.module.css";
 import Person from "@mui/icons-material/Person";
-import Typography from "@mui/joy/Typography";
+import Menu from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
-import Button from "@mui/joy/Button";
 
-interface HeaderProps {
-  setRegistrationPage: React.Dispatch<React.SetStateAction<number>>;
-}
 
 export default function Header() {
   return (
-    <div className={styles.header}>
-      <div className={styles.button}>
-      <Link href="/">
-        <div className={styles.title}>
-          PoS Sistem
+    <div className='fixed left-0 top-0 w-full ease-in duration-300 bg-slate-600'>
+      <div className='flex justify-between items-center xl:justify-center p-4 text-white'>
+        <div className='border px-5 py-2'>
+          <Link href="/">
+            <div>
+              PoS Sistem
+            </div>
+          </Link>
         </div>
-      </Link>
-      </div>
-      <div className={styles.stack}>
-        <Typography>
-          Merhaba, Hesabınıza giriş yapın
-          <Link href="/profile" className={styles.icon}>
-            <IconButton size="small">
+        <div className='block xl:hidden'>
+          <IconButton size="small" className='hover:bg-white hover:text-slate-600 text-white'>
+            <Menu />
+          </IconButton>
+        </div>
+        <div className='fixed right-0 mr-5 hidden xl:flex justify-center items-center'>
+          <h1 className='mr-4'>Merhaba, Hesabınıza giriş yapın</h1>
+          <Link href="/profile" className='mr-4'>
+            <IconButton size="small" className='hover:bg-white hover:text-slate-600 text-white'>
               <Person />
             </IconButton>
           </Link>
-          <Link href="/registration" className={styles.button}>
-              <Button color="neutral" size="sm" variant="solid">
-                Abone Olun
-              </Button>
+          <Link href="/registration" className='border px-5 py-2'>
+            Abone Olun
           </Link>
-        </Typography>
+        </div>
       </div>
     </div>
   );
